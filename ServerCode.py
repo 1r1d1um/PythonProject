a59fad0e517aff0b2738d1def9a7c7a383b340db
Anthony Mitchell <72773604+1r1d1um@users.noreply.github.com>
2020-11-29 18:10:24 -0700

    Add files via upload

--- ServerCode.py
+++ ServerCode.py
@@ -1,45 +1,48 @@
 
 import socket
 import _thread
+import json
+
+# checks through the list of connected users and sends the data to each of them (excluding the user who originally sent the data for retransmission)
+def sendAllMessage(data, clientVal):
+    for user in connectedUsers:
+        if user != clientVal:
+            try:
+                user.send(data)
+            except:
+                user.close()                    # closes user connection if data could not be sent (possibly disconnected)
+                removeConnection(clientVal)     # removes their name from the list that shows connected users
+
+def removeConnection(clientRemove):
+    if clientRemove in connectedUsers:          # checks for specified user in list to remove
+        connectedUsers.remove(clientRemove)     # removes name from list
 
 # threading allows multiple users to connect to server *THIS WILL BE MODIFIED LATER*
-def client_thread(client, address):
+def client_thread(clientThread, addressOfUser):
+    connectionString = "Connected to chatroom"
+    clientThread.send(bytes(connectionString, encoding='utf8'))
+
     while True:
-        data = client.recv(1024) # receives data from client
-        print(data) # prints it out for us to see
-        if not data: break # if no data is being recieved (blank lines), breaks out
-        reply = "no u" # "prepares a reply to send back to client on acknowledgements, may be modified."
-        reply = bytes(reply, encoding='utf8') # Makes reply into bytes. This must be done between our program to communicate correctly
-        client.send(reply) # sents a reply back
-    client.close() # closes connection
+        data = clientThread.recv(1024)      # receives data from client
+        if data:                            # checks if data has been received and stops when no more is coming in
+            print(data)                     # prints the data out on the server in the json/dictionary format. This can be modified later for better server logging
+            sendAllMessage(data, client)    # sends messages to all connected users (except original sender)
+            # client.send(data) <-- used to test if original user can also recieve messages
+        else:
+            removeConnection(clientThread)  # if no data arrives from the connected user, the user is disconnected from the server
 
-# starts up the server *THIS WILL MOST LIKELY REMAIN THE SAME EXCEPT ALLOWING MODIFICATION OF TCP_IP AND PORT (or automatic finding).
-def serverReceive():
-    TCP_IP = '192.168.1.129'
-    TCP_PORT = 5005
-    print(TCP_IP)
-    print(TCP_PORT)
+TCP_IP = input("Input IP Address of Server: ")  # default 192.168.1.129 unless it changed
+TCP_PORT = input("Input Port for Server: ")     # default 5005 and should remain constant
+print(TCP_IP)
+print(TCP_PORT)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(10)    # allows 10 users to connect, or rejects incoming connections
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((TCP_IP, int(TCP_PORT)))
+server.listen(10)                                               # allows 10 users to connect, or rejects incoming connections
+connectedUsers = []                                             # used to keep track of connected users
 
-    # loops forever for new threads to occur
-    while True:
-        c, address = s.accept()
-        _thread.start_new_thread(client_thread, (c, address))
-
-#Code below used to send message to server to test connectivity
-#def serverSend():
-#    TCP_IP = '192.168.1.129'
-#    TCP_PORT = 5005
-#    MESSAGE = 'THIS SENT SUCCESSFULLY'
-#    MESSAGE = bytes(MESSAGE, encoding='utf8')
-#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    s.connect((TCP_IP, TCP_PORT))
-#    s.send(MESSAGE)
-#    data = s.recv(1024)
-#    s.close()
-
-
-serverReceive()     # starts server up and will continue to run and recieve messages, then send a response back
+# loops forever for new threads to occur
+while True:
+    client, address = server.accept()                           # accepts new incoming connection
+    connectedUsers.append(client)                               # appends the ip address/identifier of the incoming client
+    _thread.start_new_thread(client_thread, (client, address))  # starts a new thread for the new user
