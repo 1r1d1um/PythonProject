0fa0552725fd1035bcb777295dba9ebf31db8ff5
Sam Haberkorn <haberkornsam@gmail.com>
2020-12-01 17:14:16 -0700

    Username and color settings

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import helpers
 from tkinter.font import Font
 import random
 from datetime import datetime
+import re
 
 
 def main():
@@ -17,30 +18,74 @@ def main():
     # container for message entry
     entry_canvas = Canvas(canvas, width=880, height=40, bg='#141414', highlightthickness=0)
     entry_canvas.place(x=450, y=490, anchor='s')
-    helpers.round_rectangle(entry_canvas, 0, 0, 820, 40, fill='#3c3f41')
+    helpers.round_rectangle(entry_canvas, 0, 0, 820, 40, fill='#444444')
 
     # text entry
-    text = Text(entry_canvas, width=87, height=2, bg='#3c3f41',
-                highlightthickness=0, font=Font(family="Calibri", size=15), fg='#ffffff')
+    text = Text(entry_canvas, width=87, height=2, bg='#444444',
+                highlightthickness=0, font=Font(family="Calibri", size=15), selectbackground='#666666')
     text.place(x=10, y=20, anchor='w')
 
+    # Username and profile color variables
+    username = StringVar()
+    color = StringVar()
+    color.set('#ff00f0')
+    username.set(f"User-{random.randint(1,1000)}")
+
     # send button
-    entry_canvas.create_oval(830, 0, 870, 40, fill='#3c3f41', tags='send')
+    entry_canvas.create_oval(830, 0, 870, 40, fill='#338edb', tags='send')
     entry_canvas.create_bitmap(852, 20, bitmap="@send_icon.xbm", tags='send')
 
     messages = MessageFrame(canvas, 880, 430)
     messages.place(x=10, y=10, anchor='nw')
 
-    entry_canvas.tag_bind('send', '<Button-1>', lambda e: send_message(e, text, messages))
+    entry_canvas.tag_bind('send', '<Button-1>', lambda e: send_message(e, text, messages, username, color))
+
+    menu = Menu(root)
+    settings_menu = Menu(menu, tearoff=0)
+    settings_menu.add_command(label="Profile", command=lambda: edit_profile(username, color))
+    menu.add_cascade(label="Settings", menu=settings_menu)
+    root.config(menu=menu)
 
     root.mainloop()
 
+def edit_profile(username_var, color_var):
+    window = Tk()
+    window.geometry("200x200")
+    window.title("Edit Profile")
+    window.config(bg="#141414")
 
-def send_message(event, message_box, message_frame):
+    Label(window, text="Username", bg='#141414', fg='#ffffff').place(x=100, y=10, anchor='n')
+    print(username_var)
+    entry = Text(window, width=10, height=1, font=Font(family="Calibri", size=15))
+    entry.place(x=100, y=40, anchor='n')
+    entry.insert("end-1c",username_var.get())
+
+    Label(window, text="Profile Hex Color", bg='#141414', fg='#ffffff').place(x=100, y=80, anchor='n')
+    color_entry = Text(window, width=10, height=1, font=Font(family="calibri", size=15))
+    color_entry.place(x=100, y=110, anchor='n')
+    color_entry.insert("end-1c", color_var.get())
+
+    save = Label(window, text="SAVE")
+    save.place(x=100, y=160, anchor='n')
+    save.bind("<Button-1>", lambda e: save_profile(username_var, color_var, entry, color_entry, window))
+
+def save_profile(username_var, color_var, user_entry, color_entry, window):
+    print("Saving...")
+    color = color_entry.get("1.0", "end-1c")
+    if color[0] != '#':
+        color.insert(0, '#')
+    if re.fullmatch(r'#[0-9a-fA-F]{6}', color):
+        username_var.set(user_entry.get("1.0", 'end-1c'))
+        color_var.set(color)
+        window.destroy()
+    else:
+        Label(window, text="Color must match '#xxxxxx'", fg='#ff0000',
+              bg="#141414", font=Font(family="calibri", size=15)).place(x=100, y=135, anchor='n')
+
+def send_message(event, message_box, message_frame, sender, color):
     msg = message_box.get("1.0", 'end-1c')
-    sender = "haberkornsam"
     time = datetime.now().strftime("%m/%d/%y %I:%M %p")
-    data = {"message": msg, "sender": sender, "time": time}
+    data = {"message": msg, "sender": sender.get(), "time": time, "color": color.get()}
     message_frame.add_sent_message(data)
     message_box.delete("1.0", 'end')
 
@@ -69,7 +114,7 @@ class MessageFrame(Frame):
         self.move_scrollbar = False
 
         # bind configuration changes
-        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.frame.bind("<Configure>", self.on_frame_configure)
 
         # populate sample data. Uncomment for testing
         self.populate(width, height)
@@ -89,22 +134,21 @@ class MessageFrame(Frame):
         helpers.round_rectangle(can, 0, 0, self.width - 180, height, fill="#444444")
 
         # username and timestamp
-        l = Label(can, text=f"{data.get('sender'.strip())}  " + u"\u2022" + f"  {data.get('time').strip()}",
-                  bg='#444444',
-                  font=Font(family="Calibri", size=9))
-        l.place(x=50, y=3, anchor='nw')
+        user = Label(can, text=f"{data.get('sender'.strip())}  " + u"\u2022" + f"  {data.get('time').strip()}",
+                     bg='#444444', font=Font(family="Calibri", size=9))
+        user.place(x=50, y=3, anchor='nw')
 
         # label with message
-        l = Label(can, text=data.get("message").strip(), width=80, anchor='w',
-                  bg=f"#444444", justify='left', wraplength=self.width - 240)
-        l.place(x=50, y=15, anchor='nw')
+        msg_label = Label(can, text=data.get("message").strip(), width=80, anchor='w',
+                          bg=f"#444444", justify='left', wraplength=self.width - 240,
+                          font=Font(family="Calibri"))
+        msg_label.place(x=50, y=15, anchor='nw')
 
         # profile pic area
-        pic = can.create_oval(40, 10, 10, 40, fill='#ff00f0')
+        pic = can.create_oval(40, 10, 10, 40, fill=data.get("color"), outline=data.get("color"))
         can.create_text(25, 25, text=data.get("sender")[0].upper())
 
         can.grid(row=self.frame.grid_size()[1], column=0, pady=4)
-
 
     def add_sent_message(self, data):
         self.move_scrollbar = True
@@ -122,47 +166,41 @@ class MessageFrame(Frame):
         helpers.round_rectangle(can, 150, 0, self.width - 30, height, fill=f"#444444")
 
         # username and timestamp
-        l = Label(can, text=f"You  " + u"\u2022" + f"  {data.get('time').strip()}", bg='#444444',
-                  font=Font(family="Calibri", size=9))
-        l.place(x=self.width - 80, y=3, anchor='ne')
+        user = Label(can, text=f"You  " + u"\u2022" + f"  {data.get('time').strip()}", bg='#444444',
+                     font=Font(family="Calibri", size=9))
+        user.place(x=self.width - 80, y=3, anchor='ne')
 
         # label with message
-        l = Label(can, text=data.get("message").strip(), width=80, anchor='e',
-                  bg=f"#444444", justify='left', wraplength=self.width - 240)
-        l.place(x=self.width - 80, y=15, anchor='ne')
+        msg_label = Label(can, text=data.get("message").strip(), width=80, anchor='e',
+                          bg=f"#444444", justify='left', wraplength=self.width - 240, font=Font(family="Calibri"))
+        msg_label.place(x=self.width - 80, y=15, anchor='ne')
 
         # profile pic area
-        pic = can.create_oval(self.width - 70, 10, self.width - 40, 40, fill='#ff00f0')
+        pic = can.create_oval(self.width - 70, 10, self.width - 40, 40, fill=data.get("color"), outline=data.get("color"))
         can.create_text(self.width - 55, 25, text=data.get("sender")[0].upper())
 
         can.grid(row=self.frame.grid_size()[1], column=0, pady=4)
 
-
-
-
     def populate(self, width, height):
-        '''Put in some fake data'''
+        """Put in some fake data for testing"""
         for row in range(100):
             data = {"message": f"This is the {row} row of the messages", "sender": "Haberkorn",
-                    "time": "01/12/2020 03:36 PM"}
+                    "time": "01/12/2020 03:36 PM", "color":"#ff4512"}
             if row % 3 == 0:
                 self.add_sent_message(data)
             else:
                 self.add_received_message(data)
 
         data = {"message": f"This is the last row of the messages {'ahhh ' * 120}", "sender": "SHaberkorn",
-                "time": "01/12/2020 03:36 PM"}
+                "time": "01/12/2020 03:36 PM", "color":"#554512"}
         self.add_received_message(data)
 
-
-    def onFrameConfigure(self, event):
-        '''Reset the scroll region to encompass the inner frame'''
+    def on_frame_configure(self, event):
+        """Reset the scroll region to encompass the inner frame"""
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
         if self.move_scrollbar:
             self.canvas.yview_moveto(1.0)
             self.move_scrollbar = False
-
-
 
 
 if __name__ == '__main__':
